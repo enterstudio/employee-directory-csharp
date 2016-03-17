8a368eb3cc7281d97fc1292f5f7a9e93d912357e
David Prothero <dprothero@twilio.com>
2016-03-17 13:04:05 -0700

    Sing employee lookup use case complete

---
+++ EmployeeDirectory.Web.Test/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using EmployeeDirectory.Web.Controllers;
+using EmployeeDirectory.Web.Services;
+using EmployeeDirectory.Web.Test.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+using Twilio.Mvc;
+using Twilio.TwiML.Mvc;
+
+namespace EmployeeDirectory.Web.Test.Controllers
+{
+    [TestClass]
+    public class EmployeeControllerTest
+    {
+        [TestMethod]
+        public async Task EmployeeController_Lookup_should_return_not_found_for_unknown_name()
+        {
+            var xml = await GetXmlResultFromLookupAsync("unknown");
+            var expectedMessage = string.Format(EmployeeController.NotFoundMessage, "unknown");
+            Assert.IsTrue(xml.Contains(expectedMessage));
+        }
+
+        private async Task<string> GetXmlResultFromLookupAsync(string lookupString)
+        {
+            var ctrl = GetTestController();
+            var result = (await ctrl.Lookup(new SmsRequest { Body = lookupString })) as TwiMLResult;
+            Assert.IsNotNull(result);
+
+            return GetXmlFromTwiMLResult(result);
+        }
+
+        private EmployeeController GetTestController()
+        {
+            return new EmployeeController(new EmployeeDirectoryService(TestEmployeeDbContext.GetTestDbContext()));
+        }
+
+        private string GetXmlFromTwiMLResult(TwiMLResult result)
+        {
+            var field = typeof(TwiMLResult).GetField("data", BindingFlags.NonPublic | BindingFlags.GetField | BindingFlags.Instance);
+            var xml = field.GetValue(result).ToString();
+            Trace.WriteLine(xml);
+            return xml;
+        }
+
+        [TestMethod]
+        public async Task EmployeeController_Lookup_should_return_joe_record()
+        {
+            var xml = await GetXmlResultFromLookupAsync("Joe Programmer");
+            Assert.IsTrue(xml.Contains("Joe Programmer"));
+            Assert.IsTrue(xml.Contains("joe@example.com"));
+            Assert.IsTrue(xml.Contains("joe.png"));
+        }
+    }
+}
---
+++ EmployeeDirectory.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using EmployeeDirectory.Web.Models;
+using EmployeeDirectory.Web.Services;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Twilio.Mvc;
+using Twilio.TwiML;
+using Twilio.TwiML.Mvc;
+
+namespace EmployeeDirectory.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public const string NotFoundMessage = "We could not find an employee matching '{0}'";
+        private EmployeeDirectoryService _service;
+
+        public EmployeeController() : base()
+        {
+            _service = new EmployeeDirectoryService(new EmployeeDbContext());
+        }
+
+        public EmployeeController(EmployeeDirectoryService service) : base()
+        {
+            _service = service;
+        }
+
+        // POST: Employee/Lookup
+        [HttpPost]
+        public async Task<ActionResult> Lookup(SmsRequest request)
+        {
+            var employee = (await _service.FindByNamePartialAsync(request.Body))
+                .FirstOrDefault();
+
+            var response = new TwilioResponse();
+            if (employee == null)
+            {
+                response.Message(string.Format(NotFoundMessage, request.Body));
+            }
+            else
+            {
+                response.Message($"{employee.FullName} - {employee.Email} - {employee.PhoneNumber}", new string[] { employee.ImageUrl }, null);
+            }
+            return new TwiMLResult(response);
+        }
+    }
+}
