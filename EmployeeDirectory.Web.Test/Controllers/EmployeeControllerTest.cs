320a6c8a57fa663bd8a2f8a79bc9a689c0ef29f2
David Prothero <dprothero@twilio.com>
2016-03-17 16:55:16 -0700

    Implemented multiple employee matches use case

--- EmployeeDirectory.Web.Test/Controllers/EmployeeControllerTest.cs
+++ EmployeeDirectory.Web.Test/Controllers/EmployeeControllerTest.cs
@@ -2,9 +2,13 @@
 using EmployeeDirectory.Web.Services;
 using EmployeeDirectory.Web.Test.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System.Diagnostics;
 using System.Reflection;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 using Twilio.Mvc;
 using Twilio.TwiML.Mvc;
 
@@ -13,6 +17,9 @@ namespace EmployeeDirectory.Web.Test.Controllers
     [TestClass]
     public class EmployeeControllerTest
     {
+        private HttpCookieCollection _requestCookies = new HttpCookieCollection();
+        private HttpCookieCollection _responseCookies = new HttpCookieCollection();
+
         [TestMethod]
         public async Task EmployeeController_Lookup_should_return_not_found_for_unknown_name()
         {
@@ -32,7 +39,18 @@ namespace EmployeeDirectory.Web.Test.Controllers
 
         private EmployeeController GetTestController()
         {
-            return new EmployeeController(new EmployeeDirectoryService(TestEmployeeDbContext.GetTestDbContext()));
+            var httpContext = new Mock<HttpContextBase>();
+            var request = new Mock<HttpRequestBase>();
+            var response = new Mock<HttpResponseBase>();
+
+            httpContext.Setup(c => c.Request).Returns(request.Object);
+            httpContext.Setup(c => c.Response).Returns(response.Object);
+            request.Setup(r => r.Cookies).Returns(_requestCookies);
+            response.Setup(r => r.Cookies).Returns(_responseCookies);
+
+            var ctrl = new EmployeeController(new EmployeeDirectoryService(TestEmployeeDbContext.GetTestDbContext()));
+            ctrl.ControllerContext = new ControllerContext(httpContext.Object, new RouteData(), ctrl);
+            return ctrl;
         }
 
         private string GetXmlFromTwiMLResult(TwiMLResult result)
@@ -50,6 +68,65 @@ namespace EmployeeDirectory.Web.Test.Controllers
             Assert.IsTrue(xml.Contains("Joe Programmer"));
             Assert.IsTrue(xml.Contains("joe@example.com"));
             Assert.IsTrue(xml.Contains("joe.png"));
+        }
+
+        [TestMethod]
+        public async Task EmployeeController_Lookup_should_return_multiple_records()
+        {
+            var xml = await GetXmlResultFromLookupAsync("Programmer");
+            Assert.IsTrue(xml.Contains("Joe Programmer"));
+            Assert.IsFalse(xml.Contains("joe@example.com"));
+            Assert.IsTrue(xml.Contains("Sally Programmer"));
+
+            var cookie = _responseCookies.Get(EmployeeController.CookieName);
+            Assert.IsNotNull(cookie);
+            Assert.AreEqual("1~2", cookie.Value);
+        }
+
+        [TestMethod]
+        public async Task EmployeeController_Lookup_should_return_not_found_for_numeric_but_no_cookie()
+        {
+            var xml = await GetXmlResultFromLookupAsync("1");
+            var expectedMessage = string.Format(EmployeeController.NotFoundMessage, "1");
+            Assert.IsTrue(xml.Contains(expectedMessage));
+        }
+
+        [TestMethod]
+        public async Task EmployeeController_Lookup_should_return_not_found_for_numeric_but_null_cookie()
+        {
+            _requestCookies.Add(new HttpCookie(EmployeeController.CookieName, null));
+            var xml = await GetXmlResultFromLookupAsync("1");
+            var expectedMessage = string.Format(EmployeeController.NotFoundMessage, "1");
+            Assert.IsTrue(xml.Contains(expectedMessage));
+        }
+
+        [TestMethod]
+        public async Task EmployeeController_Lookup_should_return_not_found_for_numeric_but_invalid_cookie()
+        {
+            _requestCookies.Add(new HttpCookie(EmployeeController.CookieName, "alskdfjsadf"));
+            var xml = await GetXmlResultFromLookupAsync("1");
+            var expectedMessage = string.Format(EmployeeController.NotFoundMessage, "1");
+            Assert.IsTrue(xml.Contains(expectedMessage));
+        }
+
+        [TestMethod]
+        public async Task EmployeeController_Lookup_should_return_joe_for_numeric_with_good_cookie()
+        {
+            _requestCookies.Add(new HttpCookie(EmployeeController.CookieName, "1~2"));
+            var xml = await GetXmlResultFromLookupAsync("1");
+            Assert.IsTrue(xml.Contains("Joe Programmer"));
+            Assert.IsTrue(xml.Contains("joe@example.com"));
+            Assert.IsTrue(xml.Contains("joe.png"));
+        }
+
+        [TestMethod]
+        public async Task EmployeeController_Lookup_should_return_sally_for_numeric_with_good_cookie()
+        {
+            _requestCookies.Add(new HttpCookie(EmployeeController.CookieName, "2~3~1"));
+            var xml = await GetXmlResultFromLookupAsync("1");
+            Assert.IsTrue(xml.Contains("Sally Programmer"));
+            Assert.IsTrue(xml.Contains("sally@example.com"));
+            Assert.IsTrue(xml.Contains("sally.png"));
         }
     }
 }
--- EmployeeDirectory.Web/Models/EmployeeDbContext.cs
+++ EmployeeDirectory.Web/Models/EmployeeDbContext.cs
@@ -19,6 +19,7 @@ namespace EmployeeDirectory.Web.Models
         {
             base.Seed(context);
             context.Employees.AddOrUpdate(e => e.Id, new Models.Employee { Id = 1, FullName = "David Prothero", Email = "dprothero@twilio.com", PhoneNumber = "+14155551212", ImageUrl = "http://www.prothero.com/img/portrait500.jpg" });
+            context.Employees.AddOrUpdate(e => e.Id, new Models.Employee { Id = 2, FullName = "David Smith", Email = "dsmith@twilio.com", PhoneNumber = "+14155551213", ImageUrl = "http://i.annihil.us/u/prod/marvel/i/mg/2/00/53710b14a320b.png" });
             context.SaveChanges();
         }
     }
